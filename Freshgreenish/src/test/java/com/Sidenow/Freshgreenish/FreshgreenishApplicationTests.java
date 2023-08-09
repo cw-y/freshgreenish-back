79e4d077ad5c1b0edac4ed726073d79ff52c7aba
youngyoung0 <taeyoung6873@gmail.com>
2023-08-09 23:42:20 +0900

    feat [#1] : spring initializr

---
+++ Freshgreenish/src/test/java/com/Sidenow/Freshgreenish/FreshgreenishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sidenow.Freshgreenish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshgreenishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
